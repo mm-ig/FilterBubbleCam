db4bd8f3c9817c449c3646e44cf2b80e0993d614
mm-ig <inagaki@design.inf.shizuoka.ac.jp>
2022-01-14 16:39:18 +0900

    cleening

--- static/js/js3.js
+++ static/js/js3.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
 
-    var container = $('#container'),
-        li = container.find('li');
+	var container = $('#container'),
+		li = container.find('li');
 
-    // Using the tzShutter plugin. We are giving the path
-    // to he shutter.png image (located in the plugin folder), and two
-    // callback functions.
+	// Using the tzShutter plugin. We are giving the path
+	// to he shutter.png image in the plugin folder and two
+	// callback functions.
 
-    container.tzShutter({
-        imgSrc: './../jquery.shutter/shutter.png',
-        closeCallback: function(){
+	container.tzShutter({
+		imgSrc: '../static/jquery.shutter/shutter.png',
+		closeCallback: function(){
 
-            // Cycling the visibility of the li items to
-            // create a simple slideshow.
+			// Cycling the visibility of the li items to
+			// create a simple slideshow.
 
-            li.filter(':visible:first').hide();
+			li.filter(':visible:first').hide();
 
-            if(li.filter(':visible').length == 0){
-                li.show();
-            }
+			if(li.filter(':visible').length == 0){
+				li.show();
+			}
 
-            // Scheduling a shutter open in 0.1 seconds:
-            setTimeout(function(){container.trigger('shutterOpen')},100);
-        },
-        loadCompleteCallback:function(){
-            setInterval(function(){
-                container.trigger('shutterClose');
-            },4000);
+			// Scheduling a shutter open in 0.1 seconds:
+			setTimeout(function(){container.trigger('shutterOpen')},100);
+		},
+		loadCompleteCallback:function(){
+			setInterval(function(){
+				container.trigger('shutterClose');
+			},4000);
 
-            container.trigger('shutterClose');
-        }
-    });
+			container.trigger('shutterClose');
+		}
+	});
 
 });
